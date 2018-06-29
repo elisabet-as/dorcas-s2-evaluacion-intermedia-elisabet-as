0eddfe6a79e8d184c7f898377b259d12f8d71d24
ElisabetAS <elisabet240498@gmail.com>
2018-06-29 18:26:49 +0200

    finish step 4

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var randomNumber = document.querySelector('#number');
+//variables para llamar a los elements del html
+var containtBox = document.querySelector('#number');
 
 var pushButton = document.querySelector('#button');
 
@@ -10,8 +11,24 @@ function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-console.log(getRandomNumber(100));
+//Guardo el resultado de la función en una variable
+var randomNumber = getRandomNumber(100);
+
+console.log(randomNumber);
 
 pushButton.addEventListener('click', function seeRandomNumber() {
-  console.log(randomNumber.value);
+  //Imprime en la consola el número introducido en el input
+  console.log(containtBox.value);
+
+  var numberBox = parseInt(containtBox.value);
+  //Comparación del número del input con el aleatorio y cambio de feedback
+  if (numberBox > randomNumber) {
+    boxFeedback.innerHTML = 'demasiado alto';
+  }
+  else if (numberBox < randomNumber) {
+    boxFeedback.innerHTML = 'demasiado bajo';
+  }
+  else if (numberBox === randomNumber) {
+    boxFeedback.innerHTML = '¡HAS GANADO, CAMPEONA!';
+  }
 });
